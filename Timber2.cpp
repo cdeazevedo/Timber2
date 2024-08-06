4f0628af5148ba66608ace28cf8c5603a431edb7
Christian de Azevedo <christiandeazevedo@yahoo.com>
2024-08-05 21:16:34 -0600

    moving the bee

--- Timber2.cpp
+++ Timber2.cpp
@@ -7,6 +7,9 @@
 using namespace sf;
 int main()
 {
+    // set random seed
+    //srand((int)time(0));
+    int number = (rand() % 100);
     VideoMode vm(1920, 1080);
     RenderWindow window(vm, "Timber!!!", Style::Default);
     Texture textureBackground;
@@ -14,6 +17,42 @@ int main()
     Sprite spriteBackground;
     spriteBackground.setTexture(textureBackground);
     spriteBackground.setPosition(0, 0);
+
+    const float TREE_HORIZONTAL_POSITION = 810;
+    const float TREE_VERTICAL_POSITION = 0;
+
+    Texture textureTree;
+    textureTree.loadFromFile("graphics/tree.png");
+    Sprite spriteTree;
+    spriteTree.setTexture(textureTree);
+    spriteTree.setPosition(TREE_HORIZONTAL_POSITION, TREE_VERTICAL_POSITION);
+
+    Texture textureBee;
+    textureBee.loadFromFile("graphics/bee.png");
+    Sprite beeSprite{ textureBee };
+    beeSprite.setPosition(0, 800);
+    bool beeActive = false;
+    float beeSpeed{ 0.0f };
+
+    Texture textureCloud;
+    textureCloud.loadFromFile("graphics/cloud.png");
+    Sprite spriteCloud1{ textureCloud };
+    Sprite spriteCloud2{ textureCloud };
+    Sprite spriteCloud3{ textureCloud };
+
+    spriteCloud1.setPosition(0, 0);
+    spriteCloud2.setPosition(0, 250);
+    spriteCloud3.setPosition(0, 500);
+
+    bool cloud1Active = false;
+    bool cloud2Active = false;
+    bool cloud3Active = false;
+
+    float cloud1Speed = 0.0f;
+    float cloud2Speed = 0.0f;
+    float cloud3Speed = 0.0f;
+
+    Clock clock;
 
     while (window.isOpen())
     {
@@ -25,13 +64,34 @@ int main()
         window.clear(); // clear the display
 
         // make new things to the display 
-        //  Create a texture to hold a graphic on the GPU Texture textureBackground;
-        // Load a graphic into the texture textureBackground.loadFromFile("graphics/background.png");
-        // Create a sprite Sprite spriteBackground;
-        // Attach the texture to the sprite spriteBackground.setTexture(textureBackground);
-        // Set the spriteBackground to cover the screen spriteBackground.setPosition(0,0);
+
+        Time dt = clock.restart();
+
+        // setup the bee
+        if (!beeActive) {
+            srand((int)time(0));
+            beeSpeed = (rand() % 200) + 200;
+            srand((int)time(0));
+            float height = (rand() % 500) + 500;
+            beeSprite.setPosition(2000, height);
+            beeActive = true;
+        }
+        else {
+            beeSprite.setPosition(
+                beeSprite.getPosition().x - beeSpeed * dt.asSeconds(),
+                beeSprite.getPosition().y
+            );
+
+            if (beeSprite.getPosition().x < -100) { beeActive = false; }
+        }
+
         window.draw(spriteBackground);
-        
+        window.draw(spriteCloud1);
+        window.draw(spriteCloud2);
+        window.draw(spriteCloud3);
+        window.draw(spriteTree);
+        window.draw(beeSprite);
+
         window.display(); //  draw them to the window
     }
     std::cout << "Hello World!\n";
